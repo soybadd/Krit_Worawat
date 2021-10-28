83341971b33bbc0a14eeb3bbb036b7b47038ab7a
Worawat Phaosiriphong <64011709@kmitl.ac.th>
2021-10-28 23:45:06 +0700

    Try hard

--- Bot Discord/main.py
+++ Bot Discord/main.py
@@ -1,8 +1,3 @@
-# Client ID : 901118179986776094
-# Token: OTAxMTE4MTc5OTg2Nzc2MDk0.YXLNtA._yPzLjq0WRzhZgIwh8_hNPfLqZ0
-# Permision Integer: 8
-
-
 # Import Libary Discord
 import discord
 from discord.ext import commands
